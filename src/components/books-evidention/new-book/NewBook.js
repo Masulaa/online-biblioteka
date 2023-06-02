deedd7a97bc1696b50921f4d14181e9f1131cd8a
Luka <lukamasulovic2@gmail.com>
2023-06-02 17:16:51 +0200

    ignore htis

--- src/components/books-evidention/new-book/NewBook.js
+++ src/components/books-evidention/new-book/NewBook.js
@@ -1,111 +1,102 @@
-import React from "react";
-import "./NewBook.css"
-import "../EvidentionOfBooks.css";
+import React, { useState } from "react";
+import "./NewBook.css";
+import { Link } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-import { GiHamburgerMenu} from 'react-icons/gi';
-import { TbDashboard} from 'react-icons/tb';
-import { AiOutlineProfile} from 'react-icons/ai';
-import { MdPeopleAlt} from 'react-icons/md';
-import { HiDocumentDuplicate} from 'react-icons/hi';
-import { TbArrowsLeftRight} from 'react-icons/tb';
-import { FiSettings} from 'react-icons/fi';
 
-function NovaKnjiga(){
+function NovaKnjiga() {
+  const [sadrzaj, setSadrzaj] = useState("");
 
-return(
-    <html>
-<head>
-  <title>My Site</title>
-  <link rel="stylesheet" type="text/css" href="main.css" />
-</head>
-<body>
-  <header  className="header">
-    <nav class="top-nav">
-      <div class="logo">Biblioteka</div>
-      <div class="profile">
-        <button class="create-btn">Kreiraj</button>
-        <div class="profile1"> 
-        <div class="profile-tab"></div>
-    </div>
+  const handleSadrzajChange = (value) => {
+    setSadrzaj(value);
+  };
+
+  const [activeLink, setActiveLink] = useState("Osnovni Detalji");
+
+  const handleClick = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+      <div>
+        <div>
+        <header className="header">
+      <div className="title">Nova Knjiga</div>
+      <div className="subtitle">Evidencije Knjige / Nova Knjiga</div>
+      <div className="nav-links">
+        <div
+          className={`nav-link ${activeLink === "Osnovni Detalji" ? "active" : ""}`}
+          onClick={() => handleClick("Osnovni Detalji")}
+        >
+          Osnovni Detalji
+        </div>
+        <div
+          className={`nav-link ${activeLink === "Specifikacije" ? "active" : ""}`}
+          onClick={() => handleClick("Specifikacije")}
+        >
+          Specifikacije
+        </div>
+        <div
+          className={`nav-link ${activeLink === "Multimedija" ? "active" : ""}`}
+          onClick={() => handleClick("Multimedija")}
+        >
+          Multimedija
+        </div>
       </div>
-    </nav>
-  </header>
-  
-  <div class="main-content">
-    <div className="Glavno">
-      <h1>Nova Knjiga</h1>
-        <p><a href="/">Evidencija Knjiga</a> / Nova knjiga</p>
-        <div className="line2"></div>
-         <div className="Stranica">
-          <p>Osnovne Detalji</p>
-          <p>Specifikacija</p>
-          <p>Multimedija</p>
-         </div>
-        <div className="line2"></div>
-         <div className="info">
+    </header>
+        <div>
+          <div>
             <label>Naziv Knjige</label>
-            <input className="input0"></input>
-            <label>Kratki sadrzaj</label>
-            <input className="ks input0"></input>
+            <input/>
+            <label>Kratki sadržaj</label>
+            <ReactQuill
+              value={sadrzaj}
+              onChange={handleSadrzajChange}
+            />
             <label>Izaberite kategorije</label>
-            <p>-Ovdje input-</p>
-            <label>Izaberite Zanrove</label>
-            <p>-Ovdje input-</p>
-         </div>
-         <div className="info2">
+            <select>Drama</select>
+            <label>Izaberite Žanrove</label>
+            <select>Akcija</select>
+          </div>
+          <div>
             <label>Izaberite autore</label>
-            <p>-Ovdje input-</p>
-            <label>Izdavac</label>
+            <select>Lukijan Musicki</select>
+            <label>Izdavač</label>
             <select>
-                <option></option>
-                <option></option>
-                <option></option>
-                <option></option>
+              <option>Nova Knjiga</option>
+              <option>Dzoli</option>
+              <option>Sarajevo</option>
+              <option>Mlada Nik</option>
             </select>
             <label>Godina Izdavanja</label>
             <select>
-                <option> </option>
-                <option>2023</option>
-                <option>2022</option>
-                <option>2021</option>
-                <option>2020</option>
-                <option>2019</option>
-                <option>2018</option>
-                <option>2017</option>
-                <option>2016</option>
-                <option>2015</option>
-                <option>2014</option>
-                <option>2013</option>
-                <option>2012</option>
-                <option>2011</option>
-                <option>2010</option>
+              <option> </option>
+              <option>2023</option>
+              <option>2022</option>
+              <option>2021</option>
+              <option>2020</option>
+              <option>2019</option>
+              <option>2018</option>
+              <option>2017</option>
+              <option>2016</option>
+              <option>2015</option>
+              <option>2014</option>
+              <option>2013</option>
+              <option>2012</option>
+              <option>2011</option>
+              <option>2010</option>
             </select>
-         </div>
-        <div className="line2"></div>
-    </div>
-    <aside>
-   
-   <nav class="side-nav">
-     <ul className='lista'>
-       <li><GiHamburgerMenu className="icon"/></li>
-       <li><TbDashboard className="icon"/></li>
-       <li><AiOutlineProfile className="icon"/></li>
-       <li><MdPeopleAlt className="icon"/></li>
-       <li><HiDocumentDuplicate className="icon"/></li>
-       <li><AiOutlineProfile className="icon"/></li>
-       <li><TbArrowsLeftRight className="icon"/></li>
-     </ul>
-     <span className='prav'>        </span>
-       <div className='line'></div>
-     <FiSettings className="settings"/>
-   </nav>
- </aside>
- <main>
- </main>
-</div>
-</body>
-</html>
-)
+          </div></div>
+          <div></div>
+          <div>
+            <button>Poništi</button>
+            <button>Sačuvaj</button>
+          </div>
+        </div>
+      </div>
+
+  );
 }
 
 export default NovaKnjiga;
--- src/components/books-evidention/table.js
+++ src/components/books-evidention/table.js
@@ -1,6 +1,7 @@
 import React from "react";
-import './table.css';
+import "./table.css";
 import { BiDotsVerticalRounded } from "react-icons/bi";
+
 const Table = () => {
   const data = [
     {
@@ -37,47 +38,48 @@ const Table = () => {
   ];
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            <input type="checkbox" />
-          </th>
-          <th>Naziv knjige</th>
-          <th>Autor</th>
-          <th>Kategorija</th>
-          <th>Na raspolaganju</th>
-          <th>Rezervisano</th>
-          <th>Izdato</th>
-          <th>U prekoracenju</th>
-          <th>Ukupna Kolicina</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>
               <input type="checkbox" />
-
-            </td>
-            <td>
-           {item.title} <img src="path/to/picture.jpg" alt=""/>
-            </td>
-            <td>{item.author}</td>
-            <td>{item.category}</td>
-            <td>{item.dummy1}</td>
-            <td>{item.dummy2}</td>
-            <td>{item.dummy3}</td>
-            <td>{item.dummy4}</td>
-            <td>{item.dummy5}</td>
-            <td className="tri-tacke">
-            <BiDotsVerticalRounded className="dots"/>
-            </td>
+            </th>
+            <th>Naziv knjige</th>
+            <th>Autor</th>
+            <th>Kategorija</th>
+            <th>Na raspolaganju</th>
+            <th>Rezervisano</th>
+            <th>Izdato</th>
+            <th>U prekoracenju</th>
+            <th>Ukupna količina</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                {item.title} <img src="path/to/picture.jpg" alt="" />
+              </td>
+              <td>{item.author}</td>
+              <td>{item.category}</td>
+              <td>{item.dummy1}</td>
+              <td>{item.dummy2}</td>
+              <td>{item.dummy3}</td>
+              <td>{item.dummy4}</td>
+              <td>{item.dummy5}</td>
+              <td className="tri-tacke">
+                <BiDotsVerticalRounded className="dots" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
