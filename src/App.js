8693e7ee520a758a18ded606a15a39e920868063
Marko Bejat <marko.bejat@gmail.com>
2023-06-19 17:39:24 +0200

    Kickoff with Luka

--- src/App.js
+++ src/App.js
@@ -1,50 +1,182 @@
-import './App.css';
-import LogIn from './components/account-components/LogIn';
-import SignUp from './components/account-components/SignUp';
+import { lazy, Suspense } from "react";
 
+import "./App.css";
+import LoadingSpinner from "./components/account-components/loading-spinner/LoadingSpinner"
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import EvidentionOfBooks from './components/books-evidention/EvidentionOfBooks'
+const LogIn = lazy(() => import("./components/account-components/LogIn"))
+const SignUp = lazy(() => import("./components/account-components/SignUp"))
 
-import NewBook from './components/books-evidention/new-book/NewBook';
+const EvidentionOfBooks = lazy(() =>
+  import("./components/books-evidention/EvidentionOfBooks")
+);
 
-import EditBook from './components/books-evidention/edit-book/EditBook';
+const EditBook = lazy(() =>
+  import("./components/books-evidention/edit-book/EditBook")
+);
 
-import BookDetails from './components/books-evidention/book-details/BookDetails';
-import BookEvid from './components/books-evidention/book-details/BookEvid';
-import BookMulti from './components/books-evidention/book-details/BookMulti';
-import BookSpec from './components/books-evidention/book-details/BookSpec';
+const BookDetails = lazy(() =>
+  import("./components/books-evidention/book-details/BookDetails")
+);
+const BookEvid = lazy(() =>
+  import("./components/books-evidention/book-details/BookEvid")
+);
+const BookMulti = lazy(() =>
+  import("./components/books-evidention/book-details/BookMulti")
+);
+const BookSpec = lazy(() =>
+  import("./components/books-evidention/book-details/BookSpec")
+);
 
-import StudentEvidention from './components/student/StudentEvidention'
-import NewStudent from './components/student/NewStudent';
-import LibrarianEvidention from './components/librarian/LibrarianEvidention';
-import NewLibrarian from './components/librarian/NewLibrarian';
-import AuthorEvidention from './components/author/AuthorEvidention';
-import NewAuthor from './components/author/NewAuthor';
-import LoadingSpinner from './components/account-components/loading-spinner/LoadingSpinner';
+const StudentEvidention = lazy(() =>
+  import("./components/student/StudentEvidention")
+);
+const NewStudent = lazy(() => import("./components/student/NewStudent"));
+const LibrarianEvidention = lazy(() =>
+  import("./components/librarian/LibrarianEvidention")
+);
+const NewLibrarian = lazy(() => import("./components/librarian/NewLibrarian"));
+const AuthorEvidention = lazy(() =>
+  import("./components/author/AuthorEvidention")
+);
+const NewAuthor = lazy(() => import("./components/author/NewAuthor"));
+
+const NewBook = lazy(() =>
+  import("./components/books-evidention/new-book/NewBook")
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SignUp/>} />
-        <Route path='/test' element={<LoadingSpinner/>}/>
-        <Route path='/LogIn' element={<LogIn/>}/>
-        <Route path='/EvidentionOfBooks' element={<EvidentionOfBooks/>}/>
-        <Route path='/EvidentionOfBooks/NewBook' element={<NewBook/>}/>
-        <Route path='/EvidentionOfBooks/EditBook' element={<EditBook/>}/>
-        <Route path='/EvidentionOfBooks/BookDetails' element={<BookDetails/>}/>
-        <Route path='/EvidentionOfBooks/BookDetails/Evidention' element={<BookEvid/>}/>
-        <Route path='/EvidentionOfBooks/BookDetails/Specification' element={<BookSpec/>}/>
-        <Route path='/EvidentionOfBooks/BookDetails/Multimedia' element={<BookMulti/>}/>
-        <Route path='/StudentEvidention' element={<StudentEvidention/>}/>
-        <Route path='/StudentEvidention/NewStudent' element={<NewStudent/>}/> 
-        <Route path='/LibrarianEvidention' element={<LibrarianEvidention/>}/>
-        <Route path='/LibrarianEvidention/NewLibrarian' element={<NewLibrarian/>}/> 
-        <Route path='/AuthorEvidention' element={<AuthorEvidention/>}/>
-        <Route path='/AuthorEvidention/NewAuthor' element={<NewAuthor/>}/> 
-
-
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={null}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/test"
+          element={
+            <Suspense fallback={null}>
+              <LoadingSpinner />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/LogIn"
+          element={
+            <Suspense fallback={null}>
+              <LogIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks"
+          element={
+            <Suspense fallback={null}>
+              <EvidentionOfBooks />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/NewBook"
+          element={
+            <Suspense fallbac="null">
+              <NewBook />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/EditBook"
+          element={
+            <Suspense fallback={null}>
+              <EditBook />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/BookDetails"
+          element={
+            <Suspense fallback={null}>
+              <BookDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/BookDetails/Evidention"
+          element={
+            <Suspense fallback={null}>
+              <BookEvid />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/BookDetails/Specification"
+          element={
+            <Suspense fallback={null}>
+              <BookSpec />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/EvidentionOfBooks/BookDetails/Multimedia"
+          element={
+            <Suspense fallback={null}>
+              <BookMulti />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/StudentEvidention"
+          element={
+            <Suspense fallback={null}>
+              <StudentEvidention />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/StudentEvidention/NewStudent"
+          element={
+            <Suspense fallback={null}>
+              <NewStudent />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/LibrarianEvidention"
+          element={
+            <Suspense fallback={null}>
+              <LibrarianEvidention />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/LibrarianEvidention/NewLibrarian"
+          element={
+            <Suspense fallback={null}>
+              <NewLibrarian />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/AuthorEvidention"
+          element={
+            <Suspense fallback={null}>
+              <AuthorEvidention />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/AuthorEvidention/NewAuthor"
+          element={
+            <Suspense fallback={null}>
+              <NewAuthor />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
