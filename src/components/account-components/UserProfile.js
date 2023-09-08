af2625a00cb846272d265f8631a754793968187f
Dan1loex <danilo.dubljevic666@gmail.com>
2023-09-08 11:21:17 +0200

    UserProfile

--- src/components/account-components/UserProfile.js
+++ src/components/account-components/UserProfile.js
@@ -26,32 +26,72 @@ const UserProfile = () =>{
 
 return(
     <div className={`blur ${isMenuOpen ? "blur-showed" : ""}`}>
-        <div className="book-details">
-          <div className="book-image">
+        <div className="profile-details">
+          {/* <div className="book-image">
             <img src={me.photoPath} alt="book img" className="slika" />
-          </div>
+          </div> */}
 
-          <div className="book-info">
+          <div className="profile-info">
             <div className="naslov">
               <h1 className="h1">
                 {me.name}
                 <div className="links">
-                  <Link className="evidention" to="/EvidentionOfBooks">
+                  <Link to="/EvidentionOfBooks">
                     Evidencija Knjiga&nbsp;
                   </Link>
                   /
-                  <Link
-                    className="book"
-                    to={""}
-                  >
-                    
-                  </Link>
                 </div>
               </h1>
             </div>
+            
           </div>
         </div>
+      <div className="line2"></div>
+              {/* <h4 className="category-info">Ime i prezime</h4>
+              <p>{me.name} {me.surname}</p>
+              <h4>Tip korisnika</h4>
+              <p>{me.role}</p>
+              <h4>JMBG</h4>
+              <p>{me.id}</p>
+              <h4>Email</h4>
+              <p>{me.email}</p>
+              <h4>Korisnicko ime</h4>
+              <p>{me.username}</p> */}
+    
+    <div className="details-content">
+    <div className="columns">
+      <div>
+        <div className="book-details-01">
+          <p className="category-info">Ime i prezime</p>
+          <h3 className="detail-info">{me.name} {me.surname}</h3>
+        </div>
+        <div className="book-details-01">
+          <p className="category-info">Tip korisnika</p>
+          <h3 className="detail-info">{me.role}</h3>
+        </div>
+        <div className="book-details-01">
+          <p className="category-info">JMBG</p>
+          <h3 className="detail-info">{me.id}</h3>
+        </div>
+        <div className="book-details-01">
+          <p className="category-info">Email</p>
+          <h3 className="detail-info">{me.email}</h3>
+        </div>
+        <div className="book-details-01">
+          <p className="category-info">Korisnicko ime</p>
+          <h3 className="detail-info">{me.username}</h3>
+        </div>
+      </div>
+      <div className="second-column01">
+         <div className="book-details-01"> 
+         <img src={me.photoPath}/>
+         </div> 
+      </div>
     </div>
+    </div>
+
+    </div>
+
 );
 }
 
